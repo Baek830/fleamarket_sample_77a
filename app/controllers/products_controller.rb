c276ea7fbf1573c7e4ec7975619d9c6d823f587f
mitsu17 <66157250+mitsu17@users.noreply.github.com>
2020-08-23 13:21:52 +0900

    add_header_and_footer

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,4 +1,11 @@
 class ProductsController < ApplicationController
+
   def index
   end
+
+  def search
+    @products = Products.search(params[:keyword])
+  end
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'products#index'
+  resources :products do
+    collection do
+      get 'search'
+    end
+  end
 end
