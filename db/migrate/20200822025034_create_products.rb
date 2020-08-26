12bdbec4c759af0d70059294941e34058a67d27e
Baek830 <66154033+Baek830@users.noreply.github.com>
2020-08-27 07:57:30 +0900

    category reference comment out

--- db/migrate/20200822025034_create_products.rb
+++ db/migrate/20200822025034_create_products.rb
@@ -9,7 +9,7 @@ class CreateProducts < ActiveRecord::Migration[5.2]
       t.integer :shipping_cost_id, null: false
       t.integer :shipment_date_id, null: false
       t.integer :prefecture, null: false
-      t.references :category, foreign_key: true
+      # t.references :category, foreign_key: true
       t.references :buyer
       t.references :seller, null: false
       t.timestamps
