8ef3d17274866385deca5d2e57f6090fe8269bd9
Junichiro178 <bz.kitm.fj@gmail.com>
2020-08-30 14:19:48 +0900

    Create dummy datas

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,36 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+p "create Users"
+User.create!(
+  email: 'user1@example.com',
+  password: 'aaaaaaaa',
+)
+User.create!(
+  email: 'user2@example.com',
+  password: 'aaaaaaaa',
+)
+
+p "create Products"
+Product.create!(
+  name: '鬼滅の刃 全巻セット',
+  description: '鬼滅の刃の全巻セットです',
+  price: 10000,
+  condition_id: 0,
+  shipping_cost_id: 0,
+  shipment_date_id: 0,
+  prefecture_id: 0,
+  buyer_id: 2,
+  seller_id: 1,
+)
+
+p "create Images"
+Image.create!(
+  image: open("#{Rails.root}/db/dummy_img/kimetsu.jpg"),
+  product_id: 1,
+)
+Image.create!(
+  image: open("#{Rails.root}/db/dummy_img/kimetsu_2.jpg"),
+  product_id: 1,
+)
