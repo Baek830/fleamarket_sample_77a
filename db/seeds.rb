7540df0c3380e3976f62bbd6a86fbdfe39e96a2c
Junichiro178 <bz.kitm.fj@gmail.com>
2020-09-03 07:39:38 +0900

    Changed detail page design to display product images clearly

--- db/seeds.rb
+++ db/seeds.rb
@@ -32,13 +32,21 @@ Product.create!(
   condition_id: 0,
   shipping_cost_id: 0,
   shipment_date_id: 0,
-  prefecture_id: 0,
+  prefecture_id: 1,
   category_id: 1,
   buyer_id: 2,
   seller_id: 1,
 )
 
 p "create Images"
+# Image.create!(
+#   image: open("#{Rails.root}/db/dummy_img/PAWAPURO.png"),
+#   product_id: 1,
+# )
+# Image.create!(
+#   image: open("#{Rails.root}/db/dummy_img/PAWAPURO_back.png"),
+#   product_id: 1,
+# )
 Image.create!(
   image: open("#{Rails.root}/db/dummy_img/Kingdom_1.jpeg"),
   product_id: 1,
