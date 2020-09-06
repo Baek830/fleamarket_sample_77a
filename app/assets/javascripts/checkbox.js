8a9989dcac6bf63b39be6da3fba0b9c434c980bb
Ryuichi2516 <shake.hands0011@gmail.com>
2020-09-06 19:34:50 +0900

    create_js_checkbox&error_message

--- app/assets/javascripts/checkbox.js
+++ app/assets/javascripts/checkbox.js
@@ -1,11 +1,12 @@
-$(function() {
-  let password = "#user_password";
-  let passwordconfirmation = "#user_password_confirmation"
+$(document).on('turbolinks:load', function() {
+  let password = ["#user_password","#user_password_confirmation"];
   let passcheck = "#check";
 
-  (passcheck).change(function() {
+  $(passcheck).change(function() {
     if ($(this).prop("checked")) {
-      
+      $(password.join()).attr("type","text");
+    } else {
+      $(password.join()).attr("type","password");
     }
-  })
+  });
 });
