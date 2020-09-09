2032e40ac3f0db620855895cae9b61a428611e03
Junichiro178 <bz.kitm.fj@gmail.com>
2020-09-09 20:26:14 +0900

    solve js error

--- app/assets/javascripts/favorite.js
+++ app/assets/javascripts/favorite.js
@@ -1,12 +1,19 @@
 
-function printHelloWithButton() {
-  let btn = document.getElementsByClassName(".like-btn-inner");
+// function printHelloWithButton() {
+//   let btn = document.getElementsByClassName(".like-btn-inner");
 
-  function  printHello() {
-    console.log("Hello");
-  }
+//   function  printHello() {
+//     console.log("Hello");
+//   }
 
-  btn.addEventListener("click", printHello);
-}
+//   btn.addEventListener("click", printHello);
+// }
 
-window.addEventListener("load", printHelloWithButton);
+// window.addEventListener("load", printHelloWithButton);
+
+// $(function(){
+//   $('.like-btn-inner').on('click', function(e){
+//     console.log('hoge');
+//     e.preventDefault()
+//   });
+// });
