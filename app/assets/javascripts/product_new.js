6a66f249f865006a71f54023a31e6076029e15de
Baek830 <66154033+Baek830@users.noreply.github.com>
2020-09-10 22:19:55 +0900

    js

--- app/assets/javascripts/product_new.js
+++ app/assets/javascripts/product_new.js
@@ -1,47 +1,28 @@
-$(document).on('turbolinks:load', ()=> {
-  const buildFileField = (num)=> {
-    const html = `<div data-index="${num}" class="js-file_group">
-                    <input class="js-file" type="file"
-                    name="product[images_attributes][${num}][image]"
-                    id="product_images_attributes_${num}_image"><br>
-                    <div class="js-remove">削除</div>
-                  </div>`;
-    return html;
-  }
-  const buildImg = (index, url)=> {
-    const html = `<img data-index="${index}" src="${url}" width="100px" height="100px">`;
-    return html;
-  }
-  let fileIndex = [1,2,3,4,5,6,7,8,9,10];
-  lastIndex = $('.js-file_group:last').data('index');
-  fileIndex.splice(0, lastIndex);
-
-  $('.hidden-destroy').hide();
-
-  $('#image-box').on('change', '.js-file', function(e) {
-    const targetIndex = $(this).parent().data('index');
-    const file = e.target.files[0];
-    const blobUrl = window.URL.createObjectURL(file);
-    if (img = $(`img[data-index="${targetIndex}"]`)[0]) {
-      img.setAttribute('image', blobUrl);
-    } else {
-      $('#previews').append(buildImg(targetIndex, blobUrl));
-      $('#image-box').append(buildFileField(fileIndex[0]));
-      fileIndex.shift();
-      fileIndex.push(fileIndex[fileIndex.length - 1] + 1)
-    }
-  });
-
-  $('#image-box').on('click', '.js-remove', function() {
-    const targetIndex = $(this).parent().data('index');
-    const hiddenCheck = $(`input[data-index="${targetIndex}"].hidden-destroy`);
-    if (hiddenCheck) hiddenCheck.prop('checked', true);
-
-    $(this).parent().remove();
-    $(`img[data-index="${targetIndex}"]`).remove();
-
-    if ($('.js-file').length == 0) $('#image-box').append(buildFileField(fileIndex[0]));
-  });
+$(document).on('click', '.image_upload', function() {
+  $ul = $("#previews");
+  $li = $(this).parents("li");
+  $label = $(this).parents(".upload-label");
+  $inputs = $ul.find(".image_upload");
+  var preview = $(
+    `<div class="image-preview__wapper">
+      <img class="preview">
+    </div>
+    <div class="image-preview_btn">
+      <div class="image-preview_btn_edit">編集</div>
+      <div class="image-preview_btn_delete">削除</div>
+    </div>`
+  );
+  var append_input = $(
+    `<li class="input">
+      <label class="upload-label">
+        <div class="upload-label__text">ドラッグアンドドロップ<br>またはクリックしてファイルをアップロード
+          <div class="input-area">
+            <input class="hidden image_upload" type="file">
+          </div>
+        </div>
+      </label>
+    </li>`
+  );
 });
 
 $(document).on('turbolinks:load',function(){
