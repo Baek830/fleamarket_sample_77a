7fb89c9a2b53a23a5e17bd0a563743a6a847baf5
Baek830 <66154033+Baek830@users.noreply.github.com>
2020-09-10 22:41:21 +0900

    comment out test

--- spec/factories/addresses.rb
+++ spec/factories/addresses.rb
@@ -1,18 +1,18 @@
-FactoryBot.define do
+# FactoryBot.define do
 
-  factory :DeliveryAddress do
+#   factory :DeliveryAddress do
 
-    last_name             { "山田" }
-    first_name            { "彩" }
-    phonetic_last_name    { "ヤマダ" }
-    phonetic_first_name   { "アヤ" }
-    postal_code           { Faker::Address.zip_code }
-    prefecture_id         { Faker::Number.digit }
-    city                  { Faker::Address.city }
-    ward                  { Faker::Address.street_address }
-    building              { Faker::Address.building_number }
-    phone_number          { Faker::PhoneNumber.cell_phone }
+#     last_name             { "山田" }
+#     first_name            { "彩" }
+#     phonetic_last_name    { "ヤマダ" }
+#     phonetic_first_name   { "アヤ" }
+#     postal_code           { Faker::Address.zip_code }
+#     prefecture_id         { Faker::Number.digit }
+#     city                  { Faker::Address.city }
+#     ward                  { Faker::Address.street_address }
+#     building              { Faker::Address.building_number }
+#     phone_number          { Faker::PhoneNumber.cell_phone }
 
-  end
+#   end
 
-end
+# end
