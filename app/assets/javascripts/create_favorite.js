37428608a994c70c450f8847347465ed76cb013e
Junichiro178 <bz.kitm.fj@gmail.com>
2020-09-13 16:24:28 +0900

    ADD html in js

--- app/assets/javascripts/create_favorite.js
+++ app/assets/javascripts/create_favorite.js
@@ -1,4 +1,11 @@
 $(function(){
+  function buildDislikeBtn(data){
+    var html =
+      `<button class= "default-btn product-dislike-btn red-effect">
+        <%= button_to "いいね", 'product_favorite_path(@product, pre_like)',method: :delete, class "dislike-btn-inner red-effect" %>
+      </button>`
+    return html;
+  }
   $('body').on('click', '.like-btn-inner', function(e){
     $('.like-btn-inner').removeClass( 'like-btn-inner' ).addClass( 'red-effect dislike-btn-inner' );
     $('.product-like-btn').addClass('red-effect')
@@ -12,6 +19,10 @@ $(function(){
       processData: false,
       contentType: false
     })
+      .done(function(data){
+        var html = buildDislikeBtn(data);
+        $('.product-btn-container__left').append(html)
+      })
   });
 
   $('body').on('click', '.red-effect', function(e){
@@ -19,6 +30,11 @@ $(function(){
     $('.product-like-btn').removeClass('red-effect')
     $('.heart').removeClass('fas').addClass('far')
     e.preventDefault()
+
+    var formObject = $('.button_to');
+    console.log(formObject);
+    // formObject.action = /products/8/favorites/12;
+
     var url = $('.button_to').attr('action');
     $.ajax({
       url: url,
@@ -27,5 +43,5 @@ $(function(){
       processData: false,
       contentType: false
     })
-  }); 
+  });
 });
