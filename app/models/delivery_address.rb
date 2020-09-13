fcb810b30447243dc0b8d14c9b1fe5bea824c379
Ryuichi2516 <shake.hands0011@gmail.com>
2020-09-13 09:50:29 +0900

    New validates postal_code

--- app/models/delivery_address.rb
+++ app/models/delivery_address.rb
@@ -1,6 +1,6 @@
 class DeliveryAddress < ApplicationRecord
   belongs_to :user, optional: true
-  validates :prefecture_id, :postal_code, :city, :ward, presence: true
+  validates :prefecture_id, :city, :ward, presence: true
   validates :first_name, :last_name, presence: true,
             format: {
               with: /\A[ぁ-んァ-ン一-龥]/,
@@ -10,6 +10,11 @@ class DeliveryAddress < ApplicationRecord
             format: {
               with: /\A[\p{katakana}　ー－&&[^ -~｡-ﾟ]]+\z/,
               message: "は全角カタカナのみで入力して下さい"
+            }
+  validates :postal_code,
+            format: {
+              with: /\A\d{7}\z/
+              message: "はハイフンなし7桁で入力して下さい"
             }
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :prefecture
