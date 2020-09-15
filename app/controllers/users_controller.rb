5bb05d477b2805549892d8a8df33c8f181aaa83f
Ryuichi2516 <shake.hands0011@gmail.com>
2020-09-15 22:02:07 +0900

    create_edit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,22 @@
 class UsersController < ApplicationController
+  before_action :move_to_products_index, only: [:show, :edit, :logout]
+
   def show
     @user = User.find(params[:id])
     @sale_item = Product.where(seller_id: current_user.id)
   end
 
+  def edit
+  end
+
+  def update
+  end
+
   def logout
+  end
+
+  def move_to_products_index
+    redirect_to root_path unless user_signed_in?
   end
   
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
     post 'delivery_addresses', to: 'users/registrations#create_delivery_address'
   end
 
-  resources :users, only: [:show] do
+  resources :users, only: [:show, :edit, :update] do
     member do
       get "logout"
     end
