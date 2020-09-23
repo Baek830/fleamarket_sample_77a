0211b58d898220025e6f1fffda6e0057924ecae3
Junichiro178 <bz.kitm.fj@gmail.com>
2020-09-23 20:19:21 +0900

    deleted show action in comments_controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -16,9 +16,6 @@ class CommentsController < ApplicationController
 
   end
 
-  def show
-  end
-
   private
   def comment_params
     params.require(:comment).permit(:content).merge(user_id: current_user.id, product_id: params[:product_id])
